54488f498877f7e1aa9e511aa2d4a38a74a290d7
Dina148 <gulaymamytova21@gmail.com>
2023-02-20 09:16:57 -0600

    added new class

--- src/main/java/selenium1/first_selenium.java
+++ src/main/java/selenium1/first_selenium.java
@@ -12,13 +12,25 @@ public class first_selenium {
 
 
 
-      WebDriver driver = new ChromeDriver();
-      driver.get("https://www.amazon.com/");
-      WebElement serchBoxdd = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-      Thread.sleep(3);
-      serchBoxdd.click();
-      //Select s= new Select(serchBoxdd);
-      //s.getFirstSelectedOption();
+      WebDriver driver = new ChromeDriver(); // Bring the driver
+        driver.get("http://the-internet.herokuapp.com/"); //driver has methods 1of them .get it will navigate to given URL
+        /*WebElement checboxLink = driver.findElement(By.xpath("//a[@href='/checkboxes']"));
+        Thread.sleep(3);
+        checboxLink.click();
+        WebElement firstCheckbox = driver.findElement(By.xpath("//form[@id='checkboxes']//input[1]"));
+        firstCheckbox.click();
+        WebElement secendCheckbox = driver.findElement(By.xpath("//form[@id='checkboxes']//input[2]"));
+        secendCheckbox.click();
+*/
+        //id="dropdown"
+
+        WebElement ddelement = driver.findElement(By.xpath("//a[@href='/dropdown']"));
+        ddelement.click();
+        WebElement dropDown = driver.findElement(By.xpath("//select[@id='dropdown']"));
+        dropDown.click();
+        Select select = new Select(dropDown);
+        select.selectByValue("Option 1");
+
 
 
 
